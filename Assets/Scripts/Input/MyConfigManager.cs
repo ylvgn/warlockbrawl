453fda61af1301bc6a661ad88c2e0477451922cd
guanyonghua <2966764421@qq.com>
2021-04-07 18:57:21 +0800

    新增myconfigmanager 方便調參

---
+++ Assets/Scripts/Input/MyConfigManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MyConfigManager : MonoBehaviour
+{
+    private bool isEnable = false;
+    public FloorController floorController;
+
+    void Start()
+    {
+        isEnable = true;
+        Selection.activeGameObject = gameObject;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F1))
+        {
+            isEnable = !isEnable;
+            if (isEnable) Selection.activeGameObject = gameObject;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (!isEnable) return;
+        if (Selection.activeGameObject == gameObject)
+        {
+            GUI.BeginGroup (new Rect (Screen.width - 100, 0, 100, 120));
+            float height = 20;
+            float posY = 0;
+            if (GUI.Button(new Rect(0, posY, 100, height), "FloorController"))
+            {
+                Selection.activeGameObject = floorController.gameObject;
+            }
+            posY += 20;
+            if (GUI.Button(new Rect(0, posY, 100, height), "FloorController"))
+            {
+                Selection.activeGameObject = floorController.gameObject;
+            }
+            posY += 20;
+            if (GUI.Button(new Rect(0, posY, 100, height), "FloorController"))
+            {
+                Selection.activeGameObject = floorController.gameObject;
+            }
+            posY += 20;
+            if (GUI.Button(new Rect(0, posY, 100, height), "FloorController"))
+            {
+                Selection.activeGameObject = floorController.gameObject;
+            }
+            posY += 20;
+            if (GUI.Button(new Rect(0, posY, 100, height), "FloorController"))
+            {
+                Selection.activeGameObject = floorController.gameObject;
+            }
+            posY += 20;
+            if (GUI.Button(new Rect(0, posY, 100, height), "FloorController"))
+            {
+                Selection.activeGameObject = floorController.gameObject;
+            }
+            GUI.EndGroup ();
+        } else {
+            if (GUI.Button(new Rect(0, 0, 100, 100), "Back"))
+            {
+                Selection.activeGameObject = gameObject;
+            }
+        }
+    }
+}
